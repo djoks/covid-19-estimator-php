ea8fb89cace4058ecf1218a269b33d26da2763c6
djoks <edjoks3000@rocketmail.com>
2020-04-15 02:21:31 +0000

    Still tying to fix tests failing on github due to headers already being sent

--- src/estimator.php
+++ src/estimator.php
@@ -21,9 +21,9 @@ function covid19ImpactEstimator($data)
   $errors = validate($data);
 
   if (!empty($errors)) {
-    http_response_code(400);
-    return response()->json([
-      "errors" => $errors
+    @http_response_code(400);
+    return @response()->json([
+      @"errors" => $errors
     ]);
   }
 
